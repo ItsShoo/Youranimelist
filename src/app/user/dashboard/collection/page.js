641e20b68e7993b97b9e53290d088214fb4d6d6c
rudi <rudikol2004@gmail.com>
2024-06-26 17:18:42 +0700

    change on collection

--- src/app/user/dashboard/collection/page.js
+++ src/app/user/dashboard/collection/page.js
@@ -4,35 +4,36 @@ import Link from "next/link"
 
 const Page = () => {
     return(
-        <section className="mt-4 w-full">
-            <Header title={"my collection"}/>
-            <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-                <Link href="/" className="relative border-2 border-color-accent">
-                    <Image src="" alt="" width={350} height={350} className="w-full"/>
-                    <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
-                        <h5 className="text-xl text-center">Judul Anime</h5>
-                    </div>
-                </Link>
-                <Link href="/" className="relative border-2 border-color-accent">
-                    <Image src="" alt="" width={350} height={350} className="w-full"/>
-                    <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
-                        <h5 className="text-xl text-center">Judul Anime</h5>
-                    </div>
-                </Link>
-                <Link href="/" className="relative border-2 border-color-accent">
-                    <Image src="" alt="" width={350} height={350} className="w-full"/>
-                    <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
-                        <h5 className="text-xl text-center">Judul Anime</h5>
-                    </div>
-                </Link>
-                <Link href="/" className="relative border-2 border-color-accent">
-                    <Image src="" alt="" width={350} height={350} className="w-full"/>
-                    <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
-                        <h5 className="text-xl text-center">Judul Anime</h5>
-                    </div>
-                </Link>
-            </div>
-        </section>
+        <h1 className="text-color-primary text-2xl">UNDER CONSTRUCT</h1>
+        // <section className="mt-4 w-full">
+        //     <Header title={"my collection"}/>
+        //     <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+        //         <Link href="/" className="relative border-2 border-color-accent">
+        //             <Image src="" alt="" width={350} height={350} className="w-full"/>
+        //             <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
+        //                 <h5 className="text-xl text-center">Judul Anime</h5>
+        //             </div>
+        //         </Link>
+        //         <Link href="/" className="relative border-2 border-color-accent">
+        //             <Image src="" alt="" width={350} height={350} className="w-full"/>
+        //             <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
+        //                 <h5 className="text-xl text-center">Judul Anime</h5>
+        //             </div>
+        //         </Link>
+        //         <Link href="/" className="relative border-2 border-color-accent">
+        //             <Image src="" alt="" width={350} height={350} className="w-full"/>
+        //             <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
+        //                 <h5 className="text-xl text-center">Judul Anime</h5>
+        //             </div>
+        //         </Link>
+        //         <Link href="/" className="relative border-2 border-color-accent">
+        //             <Image src="" alt="" width={350} height={350} className="w-full"/>
+        //             <div className="absolute flex justify-center items-center bottom-0 w-full bg-color-accent h-16">
+        //                 <h5 className="text-xl text-center">Judul Anime</h5>
+        //             </div>
+        //         </Link>
+        //     </div>
+        // </section>
     )
 }
 export default Page
